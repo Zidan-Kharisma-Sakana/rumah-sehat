167e4b74053d3bce5906a8d5ce84c737fcc02d39
zidan kharisma a <zidan.kharisma@ui.ac.id>
2022-11-16 18:10:39 +0700

    chore: add rest controller

--- spring/src/main/java/com/TugasAkhir/spring/controller/MainController.java
+++ spring/src/main/java/com/TugasAkhir/spring/controller/MainController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-// Notes: Please use english verb/adjective to describe your path
 @Controller
-@RequestMapping("/auth")
-public class AuthController {
+@RequestMapping("/")
+public class MainController {
+    @GetMapping("/")
+    public String Home(Model model){
+        return "home";
+    }
 }
--- spring/src/main/java/com/TugasAkhir/spring/rest_controller/InvoiceRESTController.java
+++ spring/src/main/java/com/TugasAkhir/spring/rest_controller/InvoiceRESTController.java
@@ -1,13 +1,11 @@
-package com.TugasAkhir.spring.controller;
+package com.TugasAkhir.spring.rest_controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // Notes: Please use english verb/adjective to describe your path
-@Controller
-@RequestMapping("/auth")
-public class AuthController {
+@RestController
+@RequestMapping("/api/invoice")
+public class InvoiceRESTController {
 }
