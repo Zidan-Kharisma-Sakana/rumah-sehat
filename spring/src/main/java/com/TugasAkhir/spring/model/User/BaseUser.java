c27938e84a3783f3d2050f5b516fb1bea1933faa
zidan kharisma a <zidan.kharisma@ui.ac.id>
2022-11-21 14:55:48 +0700

    fix: add auto generate uuid

--- spring/src/main/java/com/TugasAkhir/spring/model/User/BaseUser.java
+++ spring/src/main/java/com/TugasAkhir/spring/model/User/BaseUser.java
@@ -3,6 +3,7 @@ package com.TugasAkhir.spring.model.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -15,6 +16,8 @@ import javax.validation.constraints.NotNull;
 @JsonIgnoreProperties(value = {"password"}, allowSetters = true)
 public class BaseUser {
     @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String uuid;
 
     @NotNull
