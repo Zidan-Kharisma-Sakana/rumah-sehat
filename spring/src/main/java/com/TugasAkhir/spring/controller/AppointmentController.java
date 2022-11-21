64e3c091006d72d5bb49a8f6d20012611fbc38fa
Farah <farah.sausan@ui.ac.id>
2022-11-21 10:30:13 +0700

    chore: add templates

--- spring/src/main/java/com/TugasAkhir/spring/controller/AppointmentController.java
+++ spring/src/main/java/com/TugasAkhir/spring/controller/AppointmentController.java
@@ -2,23 +2,37 @@ package com.TugasAkhir.spring.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.TugasAkhir.spring.model.AppointmentModel;
+import com.TugasAkhir.spring.service.AppointmentService;
 
 
 // Notes: Please use english verb/adjective to describe your path
 @Controller
 @RequestMapping("/appointment")
 public class AppointmentController {
-
+    @Autowired
+    AppointmentService appointmentService;
+    
+    //@Autowired
+    //UserService userService;
+    
     @GetMapping(value = "/detail/{id}")
     public String detailAppointment(@PathVariable String id,Model model, Principal principal){
         AppointmentModel appointment = appointmentService.findById(id);
+        // role = ...
+
+        if(appointment==null) {
+            return "gagal-view-appointment";
+            
+        }
 
         model.addAttribute("appointment",appointment);
         return "detail-appointment";
@@ -26,8 +40,39 @@ public class AppointmentController {
 
     @PostMapping(value = "detail/{id}/save")
     public String saveAppointment(@PathVariable String id, Model model, Principal principal){
-        appointmentService.add()
-        return "detail-appointment";
+        AppointmentModel appointment = appointmentService.findById(id);
+        int status = 0;
+        if(appointment==null) {
+            status=1;
+            model.addAttribute("status", status);
+            return "gagal-save-appointment";
+            
+        }
+
+        if(appointment.getPrescription()!= null){
+            if(!appointment.getPrescription().getIsDone()){
+                status=2;
+                model.addAttribute("status", status);
+                return "gagal-save-appointment";
+            } else if (appointment.getIsDone()){
+                status=3;
+                model.addAttribute("status", status);
+                return "gagal-save-appointment";
+            }
+        } else {
+            if(appointment.getIsDone()){
+                status=3;
+                model.addAttribute("status", status);
+                return "gagal-save-appointment";
+            }
+        }
+        model.addAttribute("appointment", appointment);
+        model.addAttribute("status", status);
+        
+        appointment.setIsDone(true);
+        appointmentService.update(appointment);
+
+        return "save-appointment";
     }
 
 
--- spring/src/main/java/com/TugasAkhir/spring/controller/PrescriptionController.java
+++ spring/src/main/java/com/TugasAkhir/spring/controller/PrescriptionController.java
@@ -1,6 +1,7 @@
 package com.TugasAkhir.spring.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.TugasAkhir.spring.model.AppointmentModel;
 import com.TugasAkhir.spring.model.DrugModel;
+import com.TugasAkhir.spring.model.DrugPrescriptionModel;
 import com.TugasAkhir.spring.model.PrescriptionModel;
+import com.TugasAkhir.spring.rest_controller.AppointmentRESTController;
+import com.TugasAkhir.spring.service.AppointmentService;
 import com.TugasAkhir.spring.service.DrugService;
 import com.TugasAkhir.spring.service.PrescriptionService;
 
@@ -27,10 +34,18 @@ public class PrescriptionController {
     @Autowired
     DrugService drugService;
 
+    @Autowired
+    AppointmentService appointmentService;
+
     @GetMapping(value = "{id}/add")
     public String addPerscription(@PathVariable String id,@ModelAttribute PrescriptionModel prescription,Model model, Principal principal){
         PrescriptionModel newPrescription = new PrescriptionModel();
+        List<DrugPrescriptionModel> newList = new ArrayList<>();
+        newPrescription.setListPrescribe(newList );
+        newPrescription.getListPrescribe().add(new DrugPrescriptionModel());
+
         List<DrugModel> listDrugExisting = drugService.viewAll();
+
 
         model.addAttribute("listDrugExisting", listDrugExisting);
         model.addAttribute("prescription", newPrescription);
@@ -43,26 +58,43 @@ public class PrescriptionController {
         PrescriptionModel newPrescription = new PrescriptionModel();
         List<DrugModel> listDrugExisting = drugService.viewAll();
 
+        if(prescription.getListPrescribe() == null || prescription.getListPrescribe().size() ==0){
+            prescription.setListPrescribe(new ArrayList<>());
+        }
+        newPrescription.getListPrescribe().add(new DrugPrescriptionModel());
+
+
         model.addAttribute("listDrugExisting", listDrugExisting);
-        model.addAttribute("prescription", newPrescription);
+        model.addAttribute("prescription", prescription);
         model.addAttribute("principal",principal);
         return "form-add-prescription";
     }
 
     @GetMapping(value = "{id}/add" ,params="deleteRow")
-    public String deletePerscriptionRow(@PathVariable String id,@ModelAttribute PrescriptionModel prescription,Model model, Principal principal){
-        PrescriptionModel newPrescription = new PrescriptionModel();
+    public String deletePerscriptionRow(@PathVariable String id,@RequestParam("deleteRow") Integer row,@ModelAttribute PrescriptionModel prescription,Model model, Principal principal){
+        
         List<DrugModel> listDrugExisting = drugService.viewAll();
 
+        final Integer rowId = Integer.valueOf(row);
+        prescription.getListPrescribe().remove(rowId.intValue());
+
+
+
+
         model.addAttribute("listDrugExisting", listDrugExisting);
-        model.addAttribute("prescription", newPrescription);
+        model.addAttribute("prescription",prescription);
         model.addAttribute("principal",principal);
         return "form-add-prescription";
     }
 
-    @GetMapping(value = "{id}/add",params="save")
+    @PostMapping(value = "{id}/add",params="save")
     public String addPerscriptionSave(@PathVariable String id,@ModelAttribute PrescriptionModel prescription,Model model, Principal principal){
+        AppointmentModel appointment = appointmentService.findById(id);
+
+        
         prescriptionService.add(prescription);
+        appointment.setPrescription(prescription);
+        appointmentService.update(appointment);
 
         model.addAttribute("prescription", prescription);
         model.addAttribute("principal",principal);
