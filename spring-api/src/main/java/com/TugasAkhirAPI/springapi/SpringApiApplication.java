4184cc49a1a089d575845d33317cda98fda4d379
zidan kharisma a <zidan.kharisma@ui.ac.id>
2022-11-28 13:13:48 +0700

    chore: finish nginx on development

--- spring-api/src/main/java/com/TugasAkhirAPI/springapi/SpringApiApplication.java
+++ spring-api/src/main/java/com/TugasAkhirAPI/springapi/SpringApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("XXX Initizalize App\nInitialize App\nInitialize App");
 		SpringApplication.run(SpringApiApplication.class, args);
+		System.out.println("Initizalize App Finished\nInitialize App Finished\nInitialize App Finished");
 	}
 
 }
