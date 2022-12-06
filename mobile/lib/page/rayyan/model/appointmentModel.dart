34d6dfc99211aca2abee73ed684b41692df45ea6
Zidan Kharisma Adidarma <zidan.kharisma@ui.ac.id>
2022-12-06 16:37:50 +0700

    Rayyan/fitur 6 7

---
+++ mobile/lib/page/rayyan/model/appointmentModel.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+List<AppointmentModel> AppointmentModelFromJson(String str) =>
+    List<AppointmentModel>.from(
+        json.decode(str).map((x) => AppointmentModel.fromJson(x)));
+
+String AppointmentModelToJson(List<AppointmentModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class AppointmentModel {
+  AppointmentModel({
+    required this.doctor_name,
+    required this.start,
+    required this.isDone,
+    required this.end,
+    required this.code,
+    required this.date,
+  });
+
+  String doctor_name;
+  String start;
+  String end;
+  String date;
+  String isDone;
+  String code;
+
+  factory AppointmentModel.fromJson(Map<String, dynamic> json) =>
+      AppointmentModel(
+        doctor_name: json["doctor_name"],
+        start: json["start"],
+        end: json["end"],
+        date: json["date"],
+        code: json['code'],
+        isDone: json['isDone'],
+      );
+
+  Map<String, dynamic> toJson() => {};
+}
--- spring-api/src/main/java/com/TugasAkhirAPI/springapi/rest_controller/AppointmentRESTController.java
+++ spring-api/src/main/java/com/TugasAkhirAPI/springapi/rest_controller/AppointmentRESTController.java
@@ -1,7 +1,19 @@
 package com.TugasAkhirAPI.springapi.rest_controller;
 
+import java.security.Principal;
+import java.util.List;
+
 import java.util.NoSuchElementException;
 
+import com.TugasAkhirAPI.springapi.dto.auth.LoginRequest;
+import com.TugasAkhirAPI.springapi.dto.rayyan.AppointmentResponse;
+import com.TugasAkhirAPI.springapi.dto.rayyan.CreateAppointmentRequest;
+import com.TugasAkhirAPI.springapi.dto.rayyan.CreateAppointmentResponse;
+import com.TugasAkhirAPI.springapi.model.User.DoctorModel;
+import com.TugasAkhirAPI.springapi.model.User.PatientModel;
+import com.TugasAkhirAPI.springapi.service.AppointmentService;
+import com.TugasAkhirAPI.springapi.service.User.DoctorService;
+import com.TugasAkhirAPI.springapi.service.User.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -10,7 +22,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 import com.TugasAkhirAPI.springapi.rest.AppointmentDetail;
 import com.TugasAkhirAPI.springapi.restservice.AppointmentRestService;
 
@@ -21,6 +35,16 @@ import com.TugasAkhirAPI.springapi.restservice.AppointmentRestService;
 public class AppointmentRESTController {
     @Autowired
     AppointmentRestService appointmentRestService;
+
+    @Autowired
+    AppointmentService appointmentService;
+
+    @Autowired
+    PatientService patientService;
+
+    @Autowired
+    DoctorService doctorService;
+
     @GetMapping(value="detail/{code}")
     private AppointmentDetail detailAppointment(@PathVariable("code") String code){
         try{
@@ -29,6 +53,39 @@ public class AppointmentRESTController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Code Course" + code + " not found");
         }
         
+    }
+
+    @GetMapping(value="/doctor")
+    private List<DoctorModel> allDoctors(){
+        return doctorService.findAll();
+    }
+
+    @GetMapping(value="/all")
+    private List<AppointmentResponse> allAppointment(Principal principal){
+        try{
+            PatientModel patient = patientService.getPatientByUsername(principal.getName());
+            return appointmentService.returnAppointments(patient);
+        } catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
+        }
+    }
+
+    @PostMapping(value="/add")
+    private CreateAppointmentResponse createAppointment(@Valid @RequestBody CreateAppointmentRequest request, BindingResult bindingResult, Principal principal){
+        if(bindingResult.hasFieldErrors()){
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field"
+            );
+        }
+        try{
+            PatientModel patient = patientService.getPatientByUsername(principal.getName());
+            DoctorModel doctor = doctorService.findByUuid(request.getDoctor_uuid());
+            return appointmentService.createAppointment(request, patient, doctor);
+        } catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
+        }
     }
     
 }
