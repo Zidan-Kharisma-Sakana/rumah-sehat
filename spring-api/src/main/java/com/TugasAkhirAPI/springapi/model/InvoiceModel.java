a91dcea0000063184f4e4fb62bfe118ffbc41f7b
Farah Sausan Aulita Adinata <farah.sausan@ui.ac.id>
2022-12-12 20:04:14 +0700

    fix: fixing add prescription

--- spring-api/src/main/java/com/TugasAkhirAPI/springapi/model/InvoiceModel.java
+++ spring-api/src/main/java/com/TugasAkhirAPI/springapi/model/InvoiceModel.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import javax.annotation.Nullable;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 
@@ -20,6 +21,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class InvoiceModel {
     @Id
+    @GenericGenerator(name = "code", strategy = "com.TugasAkhirAPI.springapi.modelInvoiceCodeGenerator")
+    @GeneratedValue(generator = "code")  
+    @Column(name="code")
     private String code;
 
     @NotNull
